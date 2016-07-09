2e886c22c887569b5c86086034b3adcd2b04553e
Tom Hanley <thanley11@gmail.com>
2016-07-08 22:24:21 -0400

    Adding screen settings

---
+++ .screenlayout/stacked.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1366x768 --pos 288x1200 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output DP2 --off
